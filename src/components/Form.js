6fa7d29b85130638fde0dcf098bb1baa8b1a667f
Jihyun <hwiyu25@gmail.com>
2021-05-09 22:16:06 +0900

    ✨ Form 이벤트 생성 및 적용

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-function Form() {
+function Form({ value, onChange, onSubmit }) {
   return (
-    <form>
-      <input type="text" placeholder="확인할 이메일을 입력해주세요" />
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        placeholder="확인할 이메일을 입력해주세요"
+        value={value}
+        onChange={onChange}
+      />
       <button type="button">초기화</button>
       <button type="submit">제출</button>
     </form>
--- src/containers/EmailContainer.js
+++ src/containers/EmailContainer.js
@@ -1,12 +1,31 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { emailApi } from "../api";
 import Form from "../components/Form";
 import List from "../components/List";
 import Result from "../components/Result";
+import { formChange, formSubmit } from "../modules/emailChecker";
 
 function EmailContainer() {
+  const { value } = useSelector((state) => ({
+    value: state.emailChecker.value,
+  }));
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    dispatch(formChange(value));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { data } = await emailApi(value);
+    dispatch(formSubmit(data));
+  };
+
   return (
     <div>
-      <Form />
+      <Form value={value} onChange={handleChange} onSubmit={handleSubmit} />
       <Result />
       <List />
     </div>
