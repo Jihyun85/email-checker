47acb9b15940f17b1703a54bc206cac2c6bfd4ad
Jihyun <hwiyu25@gmail.com>
2021-05-09 20:07:47 +0900

    🏗️ EmailContainer 생성

---
+++ src/containers/EmailContainer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Form from "../components/Form";
+import List from "../components/List";
+import Result from "../components/Result";
+
+function EmailContainer() {
+  return (
+    <div>
+      <Form />
+      <Result />
+      <List />
+    </div>
+  );
+}
+
+export default EmailContainer;
