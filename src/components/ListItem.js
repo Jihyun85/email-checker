3c77e822e6473dfafcc5ccabe73b6f4c5fcfb194
Jihyun <hwiyu25@gmail.com>
2021-05-11 16:31:51 +0900

    🚧 ListItem 마크업 추가

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { MdDelete } from "react-icons/md";
+import { FaRegCheckCircle, FaRegTimesCircle } from "react-icons/fa";
 
-function ListItem() {
-  return <li></li>;
+function ListItem({ id, item, deleteList }) {
+  return (
+    <li>
+      <a href="/">{item.email}</a>
+      <p>
+        {item.smtp_check ? (
+          <>
+            <FaRegCheckCircle />
+            <p>유효합니다.</p>
+          </>
+        ) : (
+          <>
+            <FaRegTimesCircle />
+            <p>유효하지 않습니다.</p>
+          </>
+        )}
+      </p>
+      <MdDelete onClick={() => deleteList(id)} />
+    </li>
+  );
 }
 
 export default ListItem;
