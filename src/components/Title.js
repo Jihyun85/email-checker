58b6d61f8bb2ff64f1bd49d997caf843b5f5e31c
Jihyun <hwiyu25@gmail.com>
2021-05-12 16:00:09 +0900

    💄 Title 스타일, 미디어 쿼리 적용

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,7 +1,33 @@
 import React from "react";
+import styled from "styled-components";
+import { customMedia } from "../styles/customMedia";
+
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 50px;
+`;
+
+const HeaderTitle = styled.h1`
+  color: black;
+  text-transform: uppercase;
+  font-size: ${({ theme }) => theme.fontSizes.smTitle};
+
+  ${customMedia.greaterThan("smBreakpoint")`
+    font-size: ${({ theme }) => theme.fontSizes.mdTitle};
+  `}
+
+  ${customMedia.greaterThan("mdBreakpoint")`
+    font-size: ${({ theme }) => theme.fontSizes.lgTitle};
+  `}
+`;
 
 function Title() {
-  return <h1>Email Checker</h1>;
+  return (
+    <HeaderContainer>
+      <HeaderTitle>Email Checker</HeaderTitle>
+    </HeaderContainer>
+  );
 }
 
 export default Title;
