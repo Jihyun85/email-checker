97251138e8c2ee546f01caea0c932fb9038f9413
Jihyun <hwiyu25@gmail.com>
2021-05-12 15:57:12 +0900

    💄 GlobalStyle 생성

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    padding: 50px 0;
+    width: 100%;
+    font-family: 'Roboto','Nanum Gothic', sans-serif;
+  }
+  
+  ul, 
+  ol,  
+  li {
+    list-style: none;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  input, button {
+    font-family: 'Roboto','Nanum Gothic', sans-serif;
+    background-color: transparent;
+    border: none;
+    outline: none;
+  }
+`;
+
+export default GlobalStyle;
