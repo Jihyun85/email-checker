fb8503fa126efd1756085851277929a4dc860846
Jihyun <hwiyu25@gmail.com>
2021-05-13 22:07:48 +0900

    💄 Result 스타일 적용

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,36 +1,72 @@
 import React from "react";
 import { FaRegCheckCircle, FaRegTimesCircle } from "react-icons/fa";
+import styled from "styled-components";
+import { Subtitle } from "../styles/components/Subtitle";
+
+const ResultSection = styled.section`
+  margin-bottom: 50px;
+`;
+
+const ResultBox = styled.div`
+  padding: 30px;
+  background-color: ${({ theme }) => theme.colors.lightGray};
+`;
+
+const DataList = styled.dl``;
+
+const ItemBox = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const ItemTitle = styled.dt`
+  font-weight: 600;
+  &:after {
+    content: " :";
+  }
+`;
+
+const ItemData = styled.dd`
+  display: flex;
+  align-items: center;
+  margin-left: 10px;
+  padding: 5px 0;
+
+  p {
+    margin-left: 5px;
+  }
+`;
 
 function Result({ result }) {
   if (result !== null) {
     return (
-      <section className="result">
-        <h2>확인 결과</h2>
-        <div>
-          <dl>
-            <div>
-              <dt>확인 요청 이메일 주소</dt>
-              <dd>{result.email}</dd>
-            </div>
-            <div>
-              <dt>유효여부</dt>
-              <dd>
+      <ResultSection className="result">
+        <Subtitle>확인 결과</Subtitle>
+        <ResultBox>
+          <DataList>
+            <ItemBox>
+              <ItemTitle>확인 요청 이메일 주소</ItemTitle>
+              <ItemData>{result.email}</ItemData>
+            </ItemBox>
+            <ItemBox>
+              <ItemTitle>유효여부</ItemTitle>
+              <ItemData>
                 {result.smtp_check ? (
                   <>
-                    <FaRegCheckCircle />
+                    <FaRegCheckCircle style={{ color: "green" }} />
                     <p>유효합니다.</p>
                   </>
                 ) : (
                   <>
-                    <FaRegTimesCircle />
+                    <FaRegTimesCircle style={{ color: "red" }} />
                     <p>유효하지 않습니다.</p>
                   </>
                 )}
-              </dd>
-            </div>
-          </dl>
-        </div>
-      </section>
+              </ItemData>
+            </ItemBox>
+          </DataList>
+        </ResultBox>
+      </ResultSection>
     );
   }
 
