560fae44db4dba6781ac0db6f40af03280a3a5c0
Jihyun <hwiyu25@gmail.com>
2021-05-13 22:10:55 +0900

    💄 List, ListItem 스타일 적용

--- src/components/List.js
+++ src/components/List.js
@@ -1,11 +1,25 @@
 import React from "react";
+import styled from "styled-components";
+import { Subtitle } from "../styles/components/Subtitle";
 import ListItem from "./ListItem";
+import { SmallGreenBtn } from "../styles/components/Button";
+
+const ListSection = styled.section`
+  position: relative;
+`;
+
+const XlsxBtn = styled(SmallGreenBtn)`
+  position: absolute;
+  top: 0;
+  right: 0;
+`;
 
 function List({ list, deleteList }) {
   if (list.length > 0) {
     return (
-      <section className="list">
-        <h2>확인 리스트</h2>
+      <ListSection className="list">
+        <Subtitle>확인 리스트</Subtitle>
+        <XlsxBtn type="button">xlsx로 다운로드</XlsxBtn>
         {list.map((item) => (
           <ListItem
             key={item.id}
@@ -14,7 +28,7 @@ function List({ list, deleteList }) {
             deleteList={deleteList}
           />
         ))}
-      </section>
+      </ListSection>
     );
   }
   return <></>;
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,26 +1,53 @@
 import React from "react";
 import { MdDelete } from "react-icons/md";
 import { FaRegCheckCircle, FaRegTimesCircle } from "react-icons/fa";
+import { Item } from "../styles/components/Item";
+import styled from "styled-components";
+
+const CustomItem = styled(Item)`
+  justify-content: space-between;
+`;
+
+const ItemResult = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Result = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const DeleteBtn = styled.button`
+  display: flex;
+  align-items: center;
+`;
 
 function ListItem({ id, item, deleteList }) {
   return (
-    <li>
+    <CustomItem>
       <a href="/">{item.email}</a>
-      <p>
+      <ItemResult>
         {item.smtp_check ? (
-          <>
+          <Result>
             <FaRegCheckCircle />
-            <p>유효합니다.</p>
-          </>
+            <p>유효</p>
+          </Result>
         ) : (
-          <>
+          <Result>
             <FaRegTimesCircle />
-            <p>유효하지 않습니다.</p>
-          </>
+            <p>유효하지 않음</p>
+          </Result>
         )}
-      </p>
-      <MdDelete onClick={() => deleteList(id)} />
-    </li>
+      </ItemResult>
+      <DeleteBtn
+        type="button"
+        onClick={() => deleteList(id)}
+        aria-label="해당 이메일 삭제하기"
+      >
+        <MdDelete />
+      </DeleteBtn>
+    </CustomItem>
   );
 }
 
